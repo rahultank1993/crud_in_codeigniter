23f063349840f4f80a69d86656c1ee3d1c344ed9
Rahul Tank <rahultank1993@gmail.com>
2017-05-04 17:34:12 +0530

    Added JqGrid
    Added JqGrid libraries  use it in home module, remove js from footer and
    add it into header file
    
    Issue : #4

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,10 +5,13 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<title>Bootstrap 101 Template</title>
+		<title>CRUD Codeigniter</title>
 
 		<!-- Bootstrap -->
-		<link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
+		
+		<!-- JqGrid -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/jqgrid/css/ui.jqgrid-bootstrap.css"); ?>" />
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,6 +19,14 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+		<!-- JqGrid -->
+		<script type="text/javascript" src="<?php echo base_url("assets/jqgrid/js/i18n/grid.locale-en.js"); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url("assets/jqgrid/js/jquery.jqGrid.min.js"); ?>"></script>
 	</head>
 	
 	<body style = "padding-top : 70px">
